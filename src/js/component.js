ab7a017b6ae61ed28ac68a2e361d19972750ddf1
Eduardo-Vinicius-dos-Santos-Rodrigues <edunorimonido@gmail.com>
2025-01-18 18:23:14 -0300

    first commit

---
+++ src/js/component.js
@@ -0,0 +1,25 @@
+let activeButton = document.querySelector(".checked");
+
+const rateButtons = document.querySelectorAll(".rating a");
+
+rateButtons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+        if (activeButton) {
+            activeButton.classList.remove("checked");
+        }
+        button.classList.add("checked");
+        activeButton = button;
+
+        document.querySelector(".thanks-rate").innerHTML = `<p>You selected ${index + 1} out of 5</p>`;
+    });
+});
+
+const btnSubmit = document.getElementById("btn-submit");
+
+btnSubmit.addEventListener("click", () => {
+    const components = document.querySelectorAll("section.component");
+    if (activeButton) {
+        components[0].classList.remove("show");
+        components[1].classList.add("show");
+    }
+});
